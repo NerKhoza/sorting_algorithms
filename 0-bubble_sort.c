5f1514b6c632e049dfdcbfaa762be80d2a09f21c
NerKhoza <khoza.ner.142@gmail.com>
2024-01-22 10:35:23 -0800

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,12 +1,10 @@
 #include "sort.h"
 
-
 /**
  * swap - a function that swaps values
  * @a: parameter 1
  * @b: parameter 2
  */
-
 void swap(int *a, int *b)
 {
 	int temp;
@@ -16,34 +14,35 @@ void swap(int *a, int *b)
 	*b = temp;
 }
 
-/*
- * bubble_sort - a function that sorts an array of integers
+/**
+ * bubble_sort - sorts an array of integers in ascending order
  * @array: parameter 1
  * @size: parameter 2
  */
-
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
 	size_t k;
 
-	for (i = 0; i < size - 1; ++i)
+	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; ++j)
+	for (j = 0; j < size - 1 - i; j++)
+	{
+	if (array[j] > array[j + 1])
+	{
+		swap(&array[j], &array[j + 1]);
+		for (k = 0; k < size; k++)
 		{
-
-			if (array[j] > array[j + 1])
+			printf("%d", array[k]);
+			if (k < size - 1)
 			{
-			swap(&array[j], &array[j + 1]);
-
-				for (k = 0; k < size; ++k)
-				{
-					printf("%d, ", array[k]);
-				}
-			printf("\n");
+				printf(", ");
 			}
 		}
+		printf("\n");
+	}
+	}
 	}
 }
 
--- sort.h
+++ sort.h
@@ -22,5 +22,6 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void swap(int *a, int *b);
 void bubble_sort(int *array, size_t size);
+void printArray(int array[], size_t size);
 
 #endif
